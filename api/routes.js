fd4215735ac53dace799698dd110e40b4f681e16
mesutBE <gelukkig.zijn.be@gmail.com>
2020-05-17 15:53:59 +0200

    solution

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,9 @@
 // require the handlers
-_;
+const handlers = require('./handlers.js');
 const express = require('express');
 
 // build the router
-_;
+const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('files API!');
@@ -11,5 +11,20 @@ router.get('/', (req, res) => {
 
 // add routes to router
 
+router.get('/files', handlers.getFiles);
+
+// read a file
+//  called by action: fetchAndLoadFile
+router.get('/files/:name', handlers.getFilesName);
+
+// write a file
+//  called by action: saveFile
+router.post('/files/:name', handlers.postFilesName);
+
+// delete a file
+//  called by action: deleteFile
+router.delete('/files/:name', handlers.deleteFile);
+
 // export the router
-_;
+module.exports = router;
+
