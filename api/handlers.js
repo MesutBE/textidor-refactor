6ce3a50e9c90dd913d49b54e0be67460ac16e867
mesutBE <gelukkig.zijn.be@gmail.com>
2020-05-17 16:33:22 +0200

    async solution

--- api/handlers.js
+++ api/handlers.js
@@ -1,29 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 const config = require('../config');
+const util = require('util');
 
 // define FILES_DIR
 const FILES_DIR = path.join(__dirname, "../files-dev");
 
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const readDir = util.promisify(fs.readdir);
+const unlinkFile = util.promisify(fs.unlink);
+
 // declare the handlers
 const handlers = {
-    getFiles: (req, res, next) => {
-        fs.readdir(FILES_DIR, (err, list) => {
-            if (err && err.code === 'ENOENT') {
-                res.status(404).end();
-                return;
-            }
-            if (err) {
-                next(err);
-                return;
-            }
-
+    getFiles: async (req, res, next) => {
+        try {
+            const list = await readDir(FILES_DIR);
             res.json(list);
-        });
-    },
-    getFilesName: (req, res, next) => {
-        const fileName = req.params.name;
-        fs.readFile(`${FILES_DIR}/${fileName}`, 'utf-8', (err, fileText) => {
+        } catch (err) {
             if (err && err.code === 'ENOENT') {
                 res.status(404).end();
                 return;
@@ -32,18 +26,41 @@ const handlers = {
                 next(err);
                 return;
             }
+        }
+    },
+    getFilesName: async (req, res, next) => {
+        const fileName = req.params.name;
+        try {
+            const fileText = await readFile(`${FILES_DIR}/${fileName}`, "utf-8");
 
             const responseData = {
                 name: fileName,
                 text: fileText,
             };
+
             res.json(responseData);
-        });
+        } catch (err) {
+            if (err && err.code === 'ENOENT') {
+                res.status(404).end();
+                return;
+            }
+            if (err) {
+                next(err);
+                return;
+            }
+        }
     },
-    postFilesName: (req, res, next) => {
+    postFilesName: async (req, res, next) => {
         const fileName = req.params.name;
         const fileText = req.body.text;
-        fs.writeFile(`${FILES_DIR}/${fileName}`, fileText, err => {
+
+        try {
+            await writeFile(`${FILES_DIR}/${fileName}`, fileText);
+
+            // refactor hint:
+            res.redirect(303, '/api/files');
+            // handlers.getFiles(req, res, next);
+        } catch (err) {
             if (err && err.code === 'ENOENT') {
                 res.status(404).end();
                 return;
@@ -52,15 +69,18 @@ const handlers = {
                 next(err);
                 return;
             }
-
-            // refactor hint:
-            res.redirect(303, '/api/files');
-            // handlers.getFiles(req, res, next);
-        });
+        }
     },
-    deleteFile: (req, res, next) => {
+    deleteFile: async (req, res, next) => {
         const fileName = req.params.name;
-        fs.unlink(`${FILES_DIR}/${fileName}`, err => {
+
+        try {
+            await unlinkFile(`${FILES_DIR}/${fileName}`);
+
+            // refactor hint:
+            res.redirect(303, '/api/files');
+            // handlers.getFiles(req, res, next);
+        } catch (err) {
             if (err && err.code === 'ENOENT') {
                 res.status(404).end();
                 return;
@@ -69,11 +89,7 @@ const handlers = {
                 next(err);
                 return;
             }
-
-            // refactor hint:
-            res.redirect(303, '/api/files');
-            // handlers.getFiles(req, res, next);
-        });
+        }
     }
 
 };
